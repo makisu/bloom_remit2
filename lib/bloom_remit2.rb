d215f98a01fe4d7e71ab0da4b2f8ebca8c343b5e
Xavi Ablaza <xlablaza@gmail.com>
2020-09-12 15:40:39 -0700

    Result of bundle gem bloom_remit2

---
+++ lib/bloom_remit2.rb
@@ -0,0 +1,6 @@
+require "bloom_remit2/version"
+
+module BloomRemit2
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bloom_remit2_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BloomRemit2 do
+  it "has a version number" do
+    expect(BloomRemit2::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
